58bdadbbc244d03b22514dacb16fe56ca328070e
Md. Abdul Kader <abdulemail789@gmail.com>
2024-01-11 12:44:24 +0600

    started blood management project and created navbar and homepage. also added css files for both and styled them

---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import { Link } from 'react-router-dom';
+import bloodicon from '../assets/bloodicon.webp'
+import '../styles/Home.css'
+const Home = () => {
+    return (
+        <div className='pt-5 blood-background'>
+            <h2 className='font-extrabold text-4xl text-center text-white'>Donate blood give the gift of life</h2>
+            <section className='flex justify-evenly py-5 mt-5'>
+                <Link>
+                    <div className=' bg-white py-5 px-16 rounded-md'>
+                        <img src={bloodicon} className='h-16 w-16 m-auto' />
+                        <h3 className='font-bold text-xl text-center text-black'>Blood<br />Add Here</h3>
+                    </div>
+                </Link>
+                <Link>
+                    <div className=' bg-white py-5 px-16 rounded-md'>
+                        <img src={bloodicon} className='h-16 w-16 m-auto' />
+                        <h3 className='font-bold text-xl text-center text-black'>Blood<br />Add Here</h3>
+                    </div>
+                </Link>
+                <Link>
+                    <div className=' bg-white py-5 px-16 rounded-md'>
+                        <img src={bloodicon} className='h-16 w-16 m-auto' />
+                        <h3 className='font-bold text-xl text-center text-black'>Blood<br />Add Here</h3>
+                    </div>
+                </Link>
+            </section>
+        </div>
+    );
+};
+
+export default Home;
